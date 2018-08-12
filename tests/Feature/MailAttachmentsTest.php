55ae0856ea5d9f43634e60883575dec26f7229bd
Adam Marciniak <adamarc@web.de>
2018-08-12 18:00:26 +0200

    Test for mail attachments

--- tests/Feature/MailAttachmentsTest.php
+++ tests/Feature/MailAttachmentsTest.php
@@ -2,13 +2,73 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class MailAttachmentsTest extends TestCase
 {
     /** @test */
-    public function mail_attachment_can_be_send_to_route(): void
+    public function file_is_required(): void
     {
+        $this->withExceptionHandling()->signIn();
 
+        $this->post(route('api.mailAttachments'), ['file' => null])
+            ->assertSessionHasErrors(['file']);
+    }
+
+    /** @test */
+    public function an_unauthenticated_user_cannot_upload_any_files(): void
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $uploadedFile = UploadedFile::fake()->create('test.pdf');
+
+        $reponse = $this->post(route('api.mailAttachments'), ['file' => $uploadedFile])
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_upload_file(): void
+    {
+        $this->signIn($user = create(User::class), 'send_mails');
+
+        $uploadedFile = UploadedFile::fake()->create('test.pdf');
+
+        $this->post(route('api.mailAttachments'), ['file' => $uploadedFile])
+            ->assertSuccessful();
+    }
+
+    /** @test */
+    public function an_admin_can_upload_file(): void
+    {
+        $this->signInAdmin();
+
+        $uploadedFile = UploadedFile::fake()->create('test.pdf');
+
+        $this->post(route('api.mailAttachments'), ['file' => $uploadedFile])
+            ->assertSuccessful();
+    }
+
+    /** @test */
+    public function returns_valid_storage_path(): void
+    {
+        $this->signInAdmin();
+
+        $uploadedFile = UploadedFile::fake()->create('test.pdf');
+
+        $response = $this->post(route('api.mailAttachments'), ['file' => $uploadedFile])
+            ->assertSuccessful();
+
+        Storage::assertExists($response->getContent());
+    }
+
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Storage::fake();
     }
 }
