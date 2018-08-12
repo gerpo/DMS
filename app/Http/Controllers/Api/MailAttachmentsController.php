521aa25ae4cc4719bc70f6b9e3f4cd219eac2190
Adam Marciniak <adamarc@web.de>
2018-08-12 17:57:40 +0200

    Adding controller method to allow api call to delete attachments

--- app/Http/Controllers/Api/MailAttachmentsController.php
+++ app/Http/Controllers/Api/MailAttachmentsController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Storage;
 
 class MailAttachmentsController extends Controller
 {
     public function store(Request $request)
     {
-        $file = $request->validate([
+        $data = $request->validate([
             'file' => 'required|file'
         ]);
 
-        return \Storage::putFile('attachments', $file);
+        return Storage::putFile('attachments', $data['file']);
+    }
+
+    public function destroy(Request $request)
+    {
+        $data = $request->validate([
+            'paths' => 'required|array',
+            'paths.*' => 'string'
+        ]);
+
+        foreach ($data['paths'] as $path) {
+            Storage::delete($path);
+        }
     }
 }
