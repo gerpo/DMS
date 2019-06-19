687373e24cd01c07847a704aec136ad2d4611c30
Adam Marciniak <adamarc@web.de>
2019-06-20 00:18:59 +0200

    WIP

--- resources/assets/js/components/TableComponent.vue
+++ resources/assets/js/components/TableComponent.vue
@@ -1,45 +1,45 @@
 <template>
-    <div class="col-12 col-lg-10 table-responsive">
+    <div>
+        <input v-if="searchable" :placeholder="placeholder" @keyup="currentPage = 1" class="form-control my-2" title="search query"
+               type="text" v-model="search_query">
         <table class="table table-hover">
             <thead class="text-capitalize text-nowrap">
             <tr>
-                <td>{{$t('user.last_name') }}</td>
-                <td>{{ $t('user.first_name') }}</td>
-                <td>{{ $t('user.username') }}</td>
-                <td>{{ $t('user.email') }}</td>
-                <td>{{ $t('user.room') }}</td>
-                <td>{{ $t('user.house') }}</td>
-                <td>{{ $t('user.confirmed') }}</td>
+                <slot name="columns">
+                    <th v-for="column in columns">
+                        {{ column }}
+                    </th>
+                </slot>
             </tr>
             </thead>
             <tbody class="text-nowrap">
-            <tr v-for="user in users">
-                <td>{{ user.lastname }}</td>
-                <td>{{ user.firstname }}</td>
-                <td>{{ user.username }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ user.full_room }}</td>
-                <td>{{ user.house }}</td>
-                <td>{{ user.confirmed }}</td>
+            <tr v-for="(item, index) in currentItems">
+                <slot :row="item" :index="index">
+                    <td v-for="column in columns">
+                        {{ item[column] }}
+                    </td>
+                </slot>
             </tr>
             </tbody>
         </table>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
+        <div class="text-center font-weight-light my-2" v-if="currentItems.length===0">No search results found.</div>
+        <nav aria-label="table pagination d-block mx-auto" v-if="currentItems.length!==0">
+            <ul class="pagination justify-content-center">
+                <li :class="{disabled:(currentPage===1)}" class="page-item" v-if="pages > 1">
+                    <button @click="previousPage" aria-label="Previous" class="page-link">
                         <span aria-hidden="true">&laquo;</span>
                         <span class="sr-only">Previous</span>
-                    </a>
+                    </button>
                 </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
+                <li :class="{active: (currentPage===page)}" @click="currentPage=page" class="page-item"
+                    v-for="page in pages">
+                    <button class="page-link">{{ page }}</button>
+                </li>
+                <li :class="{disabled:(currentPage===pages)}" class="page-item" v-if="pages > 1">
+                    <button @click="nextPage" aria-label="Next" class="page-link">
                         <span aria-hidden="true">&raquo;</span>
                         <span class="sr-only">Next</span>
-                    </a>
+                    </button>
                 </li>
             </ul>
         </nav>
@@ -48,7 +48,59 @@
 
 <script>
     export default {
-        name: "TableComponent"
+        name: "TableComponent",
+        props: {
+            data: {default: () => [], type: [Array]},
+            columns: {default: () => [], type: [Array]},
+            itemsPerPage: {default: 50, type: [Number]},
+            searchable: {default: true, type: [Boolean]},
+            searchAttr: {type: [String]},
+            searchFunction: {type: [Function] },
+            sortBy: {default: '', type: [String]},
+            placeholder: {default: 'Search...', type: [String]},
+        },
+        data: () => ({
+            currentPage: 1,
+            search_query: '',
+            currentData: [],
+        }),
+        computed: {
+            filteredItems() {
+                if (!this.search_query) {
+                    return this.data;
+                }
+
+                if(this.searchFunction){
+                    return this.data.filter(item => this.searchFunction(item, this.search_query));
+                }
+
+                return this.data.filter(item => this.compareFunction(item[this.searchAttr]));
+            },
+            currentItems() {
+                const start = (this.currentPage - 1) * this.itemsPerPage;
+                const end = start + this.itemsPerPage;
+
+                return this.filteredItems.slice(start, end);
+            },
+            pages() {
+                return Math.ceil(this.filteredItems.length / this.itemsPerPage);
+            }
+        },
+        methods: {
+            compareFunction(value) {
+                return value.toLowerCase().includes(this.search_query.toLowerCase());
+            },
+            nextPage() {
+                if (this.currentPage < this.pages) {
+                    this.currentPage++;
+                }
+            },
+            previousPage() {
+                if (this.currentPage > 1) {
+                    this.currentPage--;
+                }
+            }
+        }
     }
 </script>
 
--- resources/assets/js/components/UserComponent.vue
+++ resources/assets/js/components/UserComponent.vue
@@ -3,136 +3,48 @@
         <div class="card-header text-capitalize">{{ $tc('user.user', 2) }}</div>
         <div class="card-body row">
 
-            <!--<table-component
-                    class="col-12 col-lg-10"
-                    :data="fetchUsers"
-                    :show-caption="false"
-                    sort-by="lastname"
-                    sort-order="asc"
-                    filter-placeholder="Search..."
-                    table-class="table table-sm table-hover text-nowrap"
-                    filter-input-class="form-control mb-2"
-
-                    ref="table"
-            >
-                <table-column show="lastname" :label="$t('user.last_name')" header-class="text-capitalize"/>
-                <table-column show="firstname" :label="$t('user.first_name')" header-class="text-capitalize"/>
-                <table-column show="username" :label="$t('user.username')" header-class="text-capitalize"/>
-                <table-column show="email" :label="$t('user.email')" header-class="text-capitalize"/>
-                <table-column show="full_room" :label="$t('user.room')" header-class="text-capitalize"/>
-                <table-column show="house" :label="$t('user.house')" header-class="text-capitalize"/>
-                <table-column show="confirmed" :label="$t('user.confirmed')" :filterable="false"
-                              header-class="text-capitalize"/>
-                <table-column :filterable="false">
-                    <template slot-scope="row">
-                        <button @click="editUser(row)" class="btn btn-link text-capitalize">Edit</button>
-                    </template>
-                </table-column>
-                <table-column :filterable="false">
-                    <template slot-scope="row">
-                        <button @click="contactUser(row)" href="#" class="btn btn-link text-capitalize">{{
-                            $t('general.contact') }}
-                        </button>
-                    </template>
-                </table-column>
-            </table-component>
-
-            <div class="sidebar col-12 col-lg-2 text-capitalize">
-                <h4>{{ $tc('general.filter', 2) }}</h4>
-
-                <hr>
-                <h5>{{ $tc('general.dorm', 2) }}</h5>
-                <ul class="filter-list">
-                    <li v-for="house in houses" class="filter-list-item">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" v-model="house.checked"
-                                   @change="filterChanged" :id="house.name">
-                            <label class="custom-control-label" :for="house.name">{{ house.name }}</label>
-                        </div>
-                    </li>
-                </ul>
-
-                <hr>
-                <h5>{{ $tc('user.user', 2) }}</h5>
-                <ul class="filter-list">
-                    <li v-for="filter in residentFilter" class="filter-list-item">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" v-model="filter.checked"
-                                   @change="filterChanged" :id="filter.name">
-                            <label class="custom-control-label" :for="filter.name">{{ $tc('user.' + filter.name, 2)
-                                }}</label>
-                        </div>
-                    </li>
-                </ul>
-            </div>-->
-
-            <!--<el-table :data="users" class="col-12 col-lg-10 text-nowrap" :filters="filters"
-                      header-row-class-name="text-capitalize">
-                <el-table-column prop="lastname" fit :label="$t('user.last_name')" sortable></el-table-column>
-                <el-table-column prop="firstname" :label="$t('user.first_name')" sortable></el-table-column>
-                <el-table-column prop="username" :label="$t('user.username')" sortable></el-table-column>
-                <el-table-column prop="email" :label="$t('user.email')" width="auto" sortable></el-table-column>
-                <el-table-column prop="full_room" :label="$t('user.room')" sortable></el-table-column>
-                <el-table-column prop="house" :label="$t('user.house')" sortable></el-table-column>
-                <el-table-column prop="confirmed" :label="$t('user.confirmed')" align="center">
-                    <template slot-scope="scope">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" v-model="scope.row.confirmed"
-                                   :id="`confirmed_${scope.$index}`" disabled>
-                            <label class="custom-control-label" :for="`confirmed_${scope.$index}`"></label>
-                        </div>
-                    </template>
-                </el-table-column>
-            </el-table>-->
             <div class="col-12 col-lg-10 table-responsive">
-                <table class="table table-hover">
-                    <thead class="text-capitalize text-nowrap">
-                    <tr>
-                        <td>{{$t('user.last_name') }}</td>
-                        <td>{{ $t('user.first_name') }}</td>
-                        <td>{{ $t('user.username') }}</td>
-                        <td>{{ $t('user.email') }}</td>
-                        <td>{{ $t('user.room') }}</td>
-                        <td>{{ $t('user.house') }}</td>
-                        <td>{{ $t('user.confirmed') }}</td>
-                    </tr>
-                    </thead>
-                    <tbody class="text-nowrap">
-                    <tr v-for="user in users">
-                        <td>{{ user.lastname }}</td>
-                        <td>{{ user.firstname }}</td>
-                        <td>{{ user.username }}</td>
-                        <td>{{ user.email }}</td>
-                        <td>{{ user.full_room }}</td>
-                        <td>{{ user.house }}</td>
-                        <td>{{ user.confirmed }}</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                <table-component :data="displayedUsers" :search-function="searchFunction">
+                    <template slot="columns">
+                        <th class="align-middle">{{$t('user.last_name') }}</th>
+                        <th class="align-middle">{{ $t('user.first_name') }}</th>
+                        <th class="align-middle">{{ $t('user.username') }}</th>
+                        <th class="align-middle">{{ $t('user.email') }}</th>
+                        <th class="align-middle">{{ $t('user.room') }}</th>
+                        <th class="align-middle">{{ $t('user.house') }}</th>
+                        <th class="align-middle">{{ $t('user.confirmed') }}</th>
+                        <th class="align-middle"></th>
+                        <th class="align-middle"></th>
+                    </template>
+                    <template slot-scope="{row, index}">
+                        <td class="align-middle">{{ row.lastname }}</td>
+                        <td class="align-middle">{{ row.firstname }}</td>
+                        <td class="align-middle">{{ row.username }}</td>
+                        <td class="align-middle">{{ row.email }}</td>
+                        <td class="align-middle">{{ row.full_room }}</td>
+                        <td class="align-middle">{{ row.house }}</td>
+                        <td class="align-middle text-center">
+                            <div class="custom-control custom-checkbox">
+                                <input :id="`confirmed_${index}`" class="custom-control-input" disabled
+                                       type="checkbox" v-model="row.confirmed">
+                                <label :for="`confirmed_${index}`" class="custom-control-label"></label>
+                            </div>
+                        </td>
+                        <td class="align-middle">
+                            <button @click="editUser(row)" class="btn btn-link text-capitalize">Edit</button>
+                        </td>
+                        <td class="align-middle">
+                            <button @click="contactUser(row)" class="btn btn-link text-capitalize">{{
+                                $t('general.contact') }}
+                            </button>
+                        </td>
+                    </template>
+                </table-component>
             </div>
 
             <div class="sidebar col-12 col-lg-2 order-first order-lg-2 text-capitalize">
-                <button class="btn btn-outline-primary d-lg-none" type="button" data-toggle="collapse"
-                        data-target="#filters" aria-expanded="false" aria-controls="filterCollapse">
+                <button aria-controls="filterCollapse" aria-expanded="false" class="btn btn-outline-primary d-lg-none"
+                        data-target="#filters" data-toggle="collapse" type="button">
                     {{ $tc('general.filter', 2) | capitalize }}
                 </button>
                 <div class="collapse d-lg-block" id="filters">
@@ -143,11 +55,11 @@
                         <div class="mr-5 mr-lg-0">
                             <h5>{{ $tc('general.dorm', 2) }}</h5>
                             <ul class="filter-list">
-                                <li v-for="house in houses" class="filter-list-item">
+                                <li class="filter-list-item" v-for="house in houses">
                                     <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" v-model="house.checked"
-                                               :id="house.name">
-                                        <label class="custom-control-label" :for="house.name">{{ house.name }}</label>
+                                        <input :id="house.name" @change="filterChanged" class="custom-control-input"
+                                               type="checkbox" v-model="house.checked">
+                                        <label :for="house.name" class="custom-control-label">{{ house.name }}</label>
                                     </div>
                                 </li>
                             </ul>
@@ -156,11 +68,11 @@
                         <div>
                             <h5>{{ $tc('user.user', 2) }}</h5>
                             <ul class="filter-list">
-                                <li v-for="filter in residentFilter" class="filter-list-item">
+                                <li class="filter-list-item" v-for="filter in residentFilter">
                                     <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" v-model="filter.checked"
-                                               :id="filter.name">
-                                        <label class="custom-control-label" :for="filter.name">{{ $tc('user.' +
+                                        <input :id="filter.name" @change="fetchUsers" class="custom-control-input"
+                                               type="checkbox" v-model="filter.checked">
+                                        <label :for="filter.name" class="custom-control-label">{{ $tc('user.' +
                                             filter.name,
                                             2)
                                             }}</label>
@@ -188,7 +100,7 @@
 </template>
 
 <script>
-    import {TableColumn, TableComponent} from 'vue-table-component';
+    import TableComponent from './TableComponent';
     import EditUserComponent from "./EditUserComponent";
     import {DataTables} from 'vue-data-tables';
 
@@ -197,12 +109,11 @@
         components: {
             EditUserComponent,
             TableComponent,
-            TableColumn,
             DataTables,
         },
         data: () => ({
             houses: [],
-            houses2: [],
+            displayedUsers: [],
             page: 1,
             filter: '',
             sort: '',
@@ -218,41 +129,40 @@
         }),
         props: {
             houseNames: {default: () => [], type: [Array]},
-            users: {default: () => [], type: [Array]},
+            usersData: {default: () => [], type: [Array]},
         },
         async created() {
+            this.displayedUsers = this.usersData;
             this.houseNames.forEach(house => {
                 this.houses.push({name: house, checked: true});
-                this.houses2.push({text: house, value: house});
             });
         },
         computed: {
             filters() {
                 return [{prop: 'house', value: this.houses.filter(house => house.checked).map(house => house.name)}];
+            },
+            totalPages() {
+                return this.displayedUsers / this.itemsPerPage;
             }
         },
         methods: {
-            async fetchUsers({page, filter, sort}) {
+            async fetchUsers() {
 
-                this.page = page;
-                this.filter = filter;
-                this.sort = sort;
-                const houses = JSON.stringify(this.houses.filter(house => !house.checked).map(house => house['name']));
                 const residentFilter = JSON.stringify(this.residentFilter.filter(filter => filter.checked).map(filter => filter['name']));
 
-                return await axios.get(route('api.users'), {params: {page, filter, sort, houses, residentFilter}})
-                    .then(response => Promise.resolve({
-                        data: response.data.data,
-                        pagination: {
-                            totalPages: response.data.last_page,
-                            currentPage: page
-                        },
-                    }));
+                await axios.get(route('api.users'), {params: {residentFilter}})
+                    .then(response =>
+                        this.displayedUsers = response.data.data);
             },
-            async filterChanged() {
-                const payload = {page: this.page, filter: this.filter, sort: this.sort};
-                return await this.fetchUsers(payload)
-                    .then(this.$refs.table.refresh());
+            filterChanged() {
+                this.displayedUsers = this.usersData.filter(user => this.houses.some(house => house.checked && user.house === house.name));
+            },
+            searchFunction(user, searchQuery) {
+                return user.lastname.toLowerCase().includes(searchQuery.toLowerCase())
+                    || user.firstname.toLowerCase().includes(searchQuery.toLowerCase())
+                    || user.email.toLowerCase().includes(searchQuery.toLowerCase())
+                    || user.username.toLowerCase().includes(searchQuery.toLowerCase())
+                    || user.full_room.toLowerCase().includes(searchQuery.toLowerCase());
             },
             editUser(user) {
                 this.selectedUser = user;
@@ -261,7 +171,7 @@
             contactUser(user) {
                 this.selectedUser = user;
                 this.showContactUserModal = true;
-            },
+            }
         },
         watch: {
             showEditUserModal(value) {
